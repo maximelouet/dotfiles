7a318019310cc49ce283e24431b4ffc62bbd18bf
Maxime Louet <saumon@maximelouet.me>
2016-08-08 18:54:44 +0200

    Handle ACPI events
    The acpihandler script is triggered by acpid on all ACPI events. The
    script locks the screen on suspend. It also sends an update signal to
    i3blocks when plugging in/out the AC adapter.

---
+++ scripts/acpihandler.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Saumon's acpi script that takes an entry for all actions
+
+case "$1" in
+    button/power)
+        case "$2" in
+            PBTN|PWRF)
+                sudo -u saumon /data/Scripts/lock.sh
+                echo -n mem >/sys/power/state
+                ;;
+        esac
+        ;;
+    button/sleep)
+        case "$2" in
+            SLPB|SBTN)
+                sudo -u saumon /data/Scripts/lock.sh
+                echo -n mem >/sys/power/state
+                ;;
+        esac
+        ;;
+    ac_adapter)
+        case "$2" in
+            AC|ACAD|ADP0|ACPI0003:00)
+                case "$4" in
+                    00000000) # unplugged
+                        pkill -RTMIN+12 i3blocks
+                        ;;
+                    00000001) # plugged
+                        pkill -RTMIN+12 i3blocks
+                        ;;
+                esac
+                ;;
+        esac
+        ;;
+    button/lid)
+        case "$3" in
+            close)
+                logger 'LID closed'
+                ;;
+            open)
+                logger 'LID opened'
+                ;;
+    esac
+    ;;
+esac
+
+# vim:set ts=4 sw=4 ft=sh et:
--- scripts/systrig/updateBattStatus.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# This script is triggered by udev when plugging in/out the AC power. This scripts sends an update signal to i3blocks in order to update the battery block
-
-pkill -RTMIN+12 i3blocks
