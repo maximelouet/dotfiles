703467de81095153497d6966b4a408ba7e1f224b
Maxime Louet <maxime.louet@epitech.eu>
2016-10-20 18:05:48 +0200

    Tek lockscreen (customized xlock matrix)

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,18 +1,4 @@
 #!/bin/sh
 
 export DISPLAY=:0
-
-img=/tmp/screenlock.png
-
-scrot $img
-convert $img -level 0%,100% \
-	-gamma 0.4,0.35,0.35 \
-	-filter Gaussian -resize 20% -define filter:sigma=0.5 -resize 500% \
-	-font "Noto-Sans" -pointsize 40 -gravity center \
-	-annotate +0+140 'Jean Pierre Foucault' -blur 0x3 \
-	-fill white -annotate +0+139 'Jean Pierre Foucault' \
-	/home/saumon/static/icons/lock.png -gravity center -composite \
-	$img
-
-i3lock -f -i $img
-
+xlock -mode matrix -timeout 5 -bg black -fg white -username "User: " -info " " -validate "Hold on..." -invalid "Nope ! " -delay 7000 +description +showdate
