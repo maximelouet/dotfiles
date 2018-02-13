47614b2f8e09750b5f921f75a15a595b6995da87
Maxime Louet <maxime@saumon.io>
2018-02-13 21:12:55 +0100

    Decrease battery_notif thresholds

--- scripts/background/battery_notif.sh
+++ scripts/background/battery_notif.sh
@@ -8,9 +8,9 @@ battStatus=$(echo $battInfo | awk '/ / {print $3}' | tr -d ',')
 if [ \( "$battStatus" != "Charging" \) ]; then
 	battCapacity=$(echo $battInfo | awk '/ / {print $4}' | tr -d '%,\n')
 	battRemain=$(echo $battInfo | awk '/ / {print $5}' | sed 's/...$//' | tr -d '\n')
-	if [ $battCapacity -lt "20" ]; then
+	if [ $battCapacity -lt "15" ]; then
 		notify-send -u critical "Very low battery" "($battCapacity%)" --icon=battery-caution
-	elif [ $battCapacity -lt "40" ]; then
+	elif [ $battCapacity -lt "30" ]; then
 		notify-send "Low battery" "($battCapacity%)" --icon=battery-low
 	fi
 fi
