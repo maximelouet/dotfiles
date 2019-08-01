a137c28d76ef34f3722dc2a3df35c832060e4d74
Maxime Louet <maxime.l@padoa-group.com>
2019-08-01 17:27:54 +0200

    Randomly select cows in Ansible

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -36,3 +36,5 @@ export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
+
+export ANSIBLE_COW_SELECTION=random
