e38c5744b30fd203f39ad723ea60b77be99a787f
Maxime Louet <maxime@saumon.io>
2026-07-23 18:32:49 +0200

    fish: improve be and bd functions
    Read interactively if no arg is provided (which makes it much easier to
    work with large amounts of data), and avoid printing huge chunks if the
    data is large, to avoid messing with the terminal or just polluting the
    output.

--- config/fish/functions/bd.fish
+++ config/fish/functions/bd.fish
@@ -1,4 +1,17 @@
 function bd --description "base64 decode"
-    echo -n "$argv" | base64 -d -w 0
-    echo -n "$argv" | base64 -d -w 0 | wl-copy -n
+    set to_decode $argv[1]
+    if test -z "$to_decode"
+        read to_decode
+    end
+    set decoded (echo -n "$to_decode" | base64 -d -w 0)
+    set decode_status $status
+    if test $decode_status -ne 0
+        return $decode_status
+    end
+    echo -n "$decoded" | wl-copy -n
+    if test (string length "$decoded") -gt 500
+        echo "Copied decoded data to clipboard."
+    else
+        echo -n "$decoded"
+    end
 end
--- config/fish/functions/be.fish
+++ config/fish/functions/be.fish
@@ -1,4 +1,17 @@
 function be --description "base64 encode"
-    echo -n "$argv" | base64 -w 0
-    echo -n "$argv" | base64 -w 0 | wl-copy -n
+    set to_encode $argv[1]
+    if test -z "$to_encode"
+        read to_encode
+    end
+    set encoded (echo -n "$to_encode" | base64 -w 0)
+    set encode_status $status
+    if test $encode_status -ne 0
+        return $encode_status
+    end
+    echo -n "$encoded" | wl-copy -n
+    if test (string length "$encoded") -gt 500
+        echo "Copied encoded data to clipboard."
+    else
+        echo -n "$encoded"
+    end
 end
