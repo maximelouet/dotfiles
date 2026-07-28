094c8c5f7a436a6089f08bf5645f22963aa214ae
Maxime Louet <maxime@saumon.io>
2026-07-28 20:30:31 +0200

    login: autostart sway on tty1 when it's installed
    No need to enumerate hostnames, this should be pretty reliable.

--- config/fish/conf.d/autostart-sway-login.fish
+++ config/fish/conf.d/autostart-sway-login.fish
@@ -1,5 +1,5 @@
 # auto-login on tty1
-if status is-login; and [ $hostname = "blahaj" ]
+if status is-login; and type -q sway
     if test -z "$WAYLAND_DISPLAY" -a "$XDG_VTNR" = 1
         exec ~/scripts/sway-user-service.sh
     end
