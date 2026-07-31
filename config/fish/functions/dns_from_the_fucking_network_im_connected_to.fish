598f2ae3f4ba39fdc2eb3d2d7cc6149f07a373c9
Maxime Louet <maxime@saumon.io>
2026-07-31 21:51:35 +0200

    fish: fix dns_ functions

--- config/fish/functions/dns_from_the_fucking_network_im_connected_to.fish
+++ config/fish/functions/dns_from_the_fucking_network_im_connected_to.fish
@@ -1,9 +1,9 @@
 function dns_from_the_fucking_network_im_connected_to
-    dns_normal
+    dns_normal --quiet
     echo "Sending DHCP Discover request with nmap..."
-    set found $(sudo nmap --script broadcast-dhcp-discover 2>&1)
-    set dns_server $(echo "$found" | grep "Domain Name Server" | awk '{print $5}')
-    if [ -z "$dns_server" ]
+    set -l found (sudo nmap --script broadcast-dhcp-discover --script-args 'timeout=5' 2>&1)
+    set -l dns_server (echo "$found" | grep -o "Domain Name Server: .* " | awk '{print $4}')
+    if [ (string length "$dns_server") -lt 9 ]
         echo "No DNS servers found. nmap reply:" >&2
         echo $found >&2
         return 1
--- config/fish/functions/dns_normal.fish
+++ config/fish/functions/dns_normal.fish
@@ -1,7 +1,6 @@
 function dns_normal --description "restore normal DNS in /etc/resolv.conf"
     sudo sed -i '/TMP dhcp_dns/d' /etc/resolv.conf
-    if [ -z "$1" ]
+    if [ -z $argv[1] ]
         cat /etc/resolv.conf
-        fi
     end
 end
